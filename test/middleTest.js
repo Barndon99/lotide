08b9a8717aaf82f8322a65a7956ea24d7b151c95
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-05-31 14:42:56 -0600

    updated tail and middle tests to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,22 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const eqArrays = require('../eqArrays');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const eqArrays = require('../eqArrays');
 const middle = require('../middle.js');
+const assert = require("chai").assert;
+
 //Test code
-assertArraysEqual(middle([]), [], true);
-assertArraysEqual(middle(["hello", "hi", "hi", "what", "are"]), ["hi"], true);
-assertArraysEqual(middle([1, 3]), [], true);
-assertArraysEqual(middle([1, 3, 6, 8, 7, 8]), [6, 8], true);
+// assertArraysEqual(middle([]), [], true);
+// assertArraysEqual(middle(["hello", "hi", "hi", "what", "are"]), ["hi"], true);
+// assertArraysEqual(middle([1, 3]), [], true);
+// assertArraysEqual(middle([1, 3, 6, 8, 7, 8]), [6, 8], true);
+
+describe("#middle", () => {
+  it("should return undefined if the array is empty", () => {
+    assert.deepEqual(middle([]), []);
+  });
+  it("should return the middle of an array", () => {
+    assert.deepEqual(middle([1, 4, 6, 7, 8]), [6]);
+  });
+  it("should return both middle numbers if the array length is even", () => {
+    assert.deepEqual(middle([1, 4, 5, 2]), [4, 5]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,17 @@
 const assertEqual = require('../assertEqual.js');
 const tail = require('../tail.js');
-
+const assert = require('chai').assert;
 //Test code
-let array = [1, 4, 8, 9, 2, 34];
-assertEqual(tail(array).length, (array.length - 1));
+// let array = [1, 4, 8, 9, 2, 34];
+// assertEqual(tail(array).length, (array.length - 1));
+// let array1 = ['hello', "hit", "buenos"];
+// assertEqual(tail(array1).length, 2);
 
-let array1 = ['hello', "hit", "buenos"];
-assertEqual(tail(array1).length, 2);
+describe("#tail", () => {
+  it("Should return everything but the first index of an array", () => {
+    assert.deepEqual(tail([1, 4, 5, 6, 7]), [4, 5, 6, 7]);
+  });
+  it("Should return an empty array if the input is an empty array", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
