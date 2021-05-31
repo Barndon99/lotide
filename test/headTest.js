1a6bd1527da826da03a0fc222b2bc84f422a9455
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-05-31 10:14:45 -0600

    added mocha testing, and cleaned up headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,21 @@
-const head = require('../head.js');
-const assertEqual = require('../assertEqual.js');
+// const assertEqual = require('../assertEqual.js');
+// const head = require('../head.js');
 
 //Test code
-assertEqual(head([5]), 5);
-assertEqual(head([]));
+// assertEqual(head([5]), 5);
+// assertEqual(head([]));
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns undefined when given an empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
